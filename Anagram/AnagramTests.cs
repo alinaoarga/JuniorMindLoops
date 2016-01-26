7e9cf864aaa2c7d10b7253484272e20dab450895
alinaoarga <oarga_alina@yahoo.com>
2016-01-26 13:53:57 +0200

    Adjusted Anagram Project

--- Anagram/AnagramTests.cs
+++ Anagram/AnagramTests.cs
@@ -44,13 +44,54 @@ namespace Anagram
         [TestMethod]
         public void AnagramAlinaTest()
         {
-            Assert.AreEqual(60, CalculateAnagram("Alina"));
+            Assert.AreEqual(60, CalculateAnagram("alina"));
         }
         [TestMethod]
         public void AnagramAndreiTest()
         {
             Assert.AreEqual(720, CalculateAnagram("Andrei"));
         }
+        [TestMethod]
+        public void UniqueABCTest()
+        {
+            Assert.AreEqual("abc", RemoveCharacters("aabbbcc"));
+        }
+        [TestMethod]
+        public void UniqueAlinaTest()
+        {
+            Assert.AreEqual("Alina", RemoveCharacters("AAlinaaa"));
+        }
+        [TestMethod]
+        public void UniqueABCDTest()
+        {
+            Assert.AreEqual("abcd", RemoveCharacters("aaaabbbccccdddddd"));
+        }
+        [TestMethod]
+        public void CountATest()
+        {
+            Assert.AreEqual(4, CountOccurrence("aaaabbbccccdddddd", 'a'));
+        }
+        [TestMethod]
+        public void CountBTest()
+        {
+            Assert.AreEqual(3, CountOccurrence("aaaabbbccccdddddd", 'b'));
+        }
+        [TestMethod]
+        public void CountCTest()
+        {
+            Assert.AreEqual(5, CountOccurrence("aaaabbbcccccdddddd", 'c'));
+        }
+        [TestMethod]
+        public void CountAlinasTest()
+        {
+            Assert.AreEqual(3, CountOccurrence("aalina", 'a'));
+        }
+        [TestMethod]
+        public void AnagramAndreeaTest()
+        {
+            Assert.AreEqual(1260, CalculateAnagram("andreea"));
+        }
+
         int CalculateFactorial(int number)
         {
             int fact = 1;
@@ -60,18 +101,54 @@ namespace Anagram
             }
             return fact;
         }
+        int CountOccurrence(string word, char character)
+        {
+            int i = 0;
+            int count = 0;
+
+            while ((i = word.IndexOf(character, i)) != -1)
+            {
+
+                count++;
+                i++;
+            }
+
+            return count;
+        }
+        string RemoveCharacters(string word)
+        {
+
+            string unique = "";
+            foreach (char letter in word)
+            {
+                if (unique.IndexOf(letter) == -1)
+                {
+                    unique += letter;
+                }
+
+            }
+            return unique;
+        }
+       
         int CalculateAnagram(string word)
         {
-           
-            int common = 0;
-            for (int i = 0; i < word.Length; i++)
+
+            char[] letters = RemoveCharacters(word).ToCharArray();
+                int prod = 1;
+            
+            int[] array = new int[500];
+            int i = 0;
+           foreach (char letter in letters)
             {
-                if (word[i].Equals(word[i]))
-                    break;
-                common++;
-            }
-            int rez = CalculateFactorial(word.Length) / CalculateFactorial(common); 
-            return rez;
+                array [i] = CountOccurrence(word, letter);
+              
+                if (array.Length > 1)
+                {
+                    prod *= CalculateFactorial(array[i]);
+                    i++;
+                }  
+           }
+            return CalculateFactorial(word.Length) / prod;
         }
     }
 }
