cd553a87c81c4ef80407b4842da33a080f4faafe
alinaoarga <oarga_alina@yahoo.com>
2016-02-18 09:51:03 +0200

    Lunch Tests

---
+++ Lunch/LunchTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch
+{
+    [TestClass]
+    public class LunchTests
+    {
+        [TestMethod]
+        public void TrueMeetingTest()
+        {
+            Assert.AreEqual(true, MeetingOccurrence(12));
+        }
+        [TestMethod]
+        public void FalseMeetingTest()
+        {
+            Assert.AreEqual(false, MeetingOccurrence(16));
+        }
+        [TestMethod]
+        public void MeetingTest()
+        {
+            Assert.AreEqual(true, MeetingOccurrence(24));
+        }
+        bool MeetingOccurrence(int day)
+        {
+            if (day % 4 == 0 && day % 6 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
