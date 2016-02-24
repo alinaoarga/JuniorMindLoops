2e807a04b589979d3f0c4ccceff65fccdef700bd
alinaoarga <oarga_alina@yahoo.com>
2016-02-24 12:06:51 +0200

    Cube Tests

---
+++ Cube/CubeTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cube
+{
+    [TestClass]
+    public class CubeTests
+    {
+        [TestMethod]
+        public void NumberForK1Test()
+        {
+            Assert.AreEqual(192, DetermineNumber(1));
+        }
+        [TestMethod]
+        public void NumberForK2Test()
+        {
+            Assert.AreEqual(442, DetermineNumber(2));
+        }
+        [TestMethod]
+        public void NumberForK3Test()
+        {
+            Assert.AreEqual(692, DetermineNumber(3));
+        }
+        int DetermineNumber(int k)
+        {
+            int number = 0;
+          if(k >= 0)
+            {
+                number = 10 * (19 + 25 * (k-1)) + 2;
+                k++;
+            }
+            return number;
+        }
+      
+    }
+}
